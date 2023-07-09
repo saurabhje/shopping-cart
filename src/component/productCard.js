8ac4b7468e0d4d600572728f05dd4f89c47d8b77
saurabhje <work.saurabh@outlook.com>
2023-07-10 03:11:09 +0530

    Fixing the product card

---
+++ src/component/productCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ const productCard = () => {
+  return (
+    // <div className="productcard">
+    //     <img className="product-img" src={props.img} alt='product-image' />
+    //     <div className='title'>{props.title}</div>
+    //     <p className='price'>{props.price}</p>
+    //     This is a productCard
+    // </div>
+    <>
+        <p> This is a product card</p>
+    </>
+  );
+}
+
+export default productCard;
--- src/component/shoppage.js
+++ src/component/shoppage.js
@@ -1,7 +1,25 @@
+import React from "react";
+import productCard from "./productCard";
+import gpu from "../images/4070.jpg"
+import iphone from "../images/iphone.jpg";
+import moduleName from '../images/airpodes.jpg';
+import ipad from '../images/ipad.jpg';
+import mac from "../images/mac.jpg";
+
 const ShopPage = () =>{
     return (
         <div>
-            <div className="cards"></div>
+            <div className="card-container">
+                <p> This is the shopPage</p>
+                <productCard  />
+                <productCard />
+                <productCard />
+                <productCard />
+                <productCard />
+                <productCard />
+                <productCard />
+                <productCard />
+            </div>
         </div>
     )
 }
